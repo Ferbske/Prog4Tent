5e6b3b4096a02e9e8e57ac3c75423ddff3de6721
Unknown <rick.voesenek@hotmail.com>
2018-05-14 13:31:41 +0200

    Poort aangepast naar 666
    8080 werkte niet bij Rick ivm dat Xampp draait op 8080

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.all('*', (req, res, next) => {
 
 app.use('/apiv1', require('./routes/apiv1'));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 666;
 
 app.listen(port, () => {
     console.log('The magic happens at http://localhost:' + port);
