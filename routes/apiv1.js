fb6f504a5e83911ed510bcb68386f6188c6cd587
Kimmy :D <kimvg2@hotmail.com>
2018-05-14 15:47:59 +0200

    Studentenhuis maaltijden
    Maaltijden per studentenhuis en specifieke maaltijd info

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -9,14 +9,14 @@ router.get('/', (req, res) => {
     res.send('Hello Avans!');
 });
 
-router.get('/studentenhuis/:naam?', (req,res) => {
+router.get('/studentenhuis/:naam?', (req, res) => {
     let naam = req.params.naam || '';
     let result;
-    if (naam === '' ){
+    if (naam === '') {
         result = studentenhuis;
     } else {
-        result = studentenhuis.filter( (user) => {
-            if (user.naam === naam){
+        result = studentenhuis.filter((user) => {
+            if (user.naam === naam) {
                 return user;
             }
         })
@@ -26,14 +26,54 @@ router.get('/studentenhuis/:naam?', (req,res) => {
 });
 
 
-router.all('/version', (req,res) => {
+router.get('/studentenhuis/:huisId?/maaltijd/:maaltijdId?', (req, res) => {
+
+    let huisId = req.params.huisId;
+    let maaltijdId = req.params.maaltijdId;
+    let result;
+    if (maaltijdId === undefined) {
+        result = maaltijd.filter((result) => {
+            if (result.studentenhuisid === huisId) {
+                return result;
+            }
+        })
+    } else {
+        result = maaltijd.filter((result) => {
+            if (result.studentenhuisid === huisId && result.id === maaltijdId) {
+                return result;
+            }
+        })
+    }
+
+    // res.send('WERKT GEWOON');
+    // }
+    // else if (huisId !== '' && maaltijdId !== '') {
+    //     result = maaltijd.filter( (result) => {
+    //         if (result.studentenhuisid === huisId && result.id === maaltijdId) {
+    //             return result;
+    //         }
+    //     })
+    // }
+
+    if (result === undefined) {
+        res.status(404);
+        res.send('Niet gevonden (huisId bestaat niet)');
+    } else {
+        res.status(200);
+        res.json(result);
+    }
+
+});
+
+
+router.all('/version', (req, res) => {
     res.status(500);
     res.json({
         "description": "Thank you for using API version 1"
     })
 });
 
-router.all('*', (req,res) => {
+router.all('*', (req, res) => {
     res.status(404);
     res.send('404 - Not found');
 });
