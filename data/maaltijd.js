faabebd501fa958c3a222af00b4e6907a30fbe88
Unknown <rick.voesenek@hotmail.com>
2018-05-14 13:18:07 +0200

    Data directory gemaakt + tijdelijke testdata

---
+++ data/maaltijd.js
@@ -0,0 +1,32 @@
+var maaltijden = [
+    {
+        id: '1',
+        naam: 'Zuurkool met worst',
+        beschrijving: 'Zuurkool a la Montizaan',
+        ingredienten: 'Zuurkool',
+        allergie: 'Lactose',
+        prijs: '5',
+        userid: '1',
+        studentenhuisid: '1'
+    },
+    {
+        id: '2',
+        naam: 'Banaan',
+        beschrijving: 'Banaan',
+        ingredienten: 'Peer',
+        allergie: 'Frits',
+        prijs: '5',
+        userid: '2',
+        studentenhuisid: '2'
+    },
+    {
+        id: '3',
+        naam: 'Pasgetti',
+        beschrijving: 'vla',
+        ingredienten: 'pudding',
+        allergie: 'kaas',
+        prijs: '5',
+        userid: '3',
+        studentenhuisid: '3'
+    }
+];
