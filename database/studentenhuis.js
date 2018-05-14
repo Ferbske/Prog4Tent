01a7881e202fb97e87447da9bfd06eecb3a10ebe
Unknown <rick.voesenek@hotmail.com>
2018-05-14 15:48:30 +0200

    DB Connector added + rename directory data -> database

--- database/studentenhuis.js
+++ database/studentenhuis.js
@@ -1,20 +1,23 @@
 var studentenhuizen = [
     {
         id: '1',
-        naam: 'Lovensdijk',
+        naam: 'naam1',
         adres: 'Breda',
+        contact: 'contact1',
         userid: '1'
     },
     {
         id: '2',
-        naam: 'Hubertuslaan',
+        naam: 'naam2',
         adres: 'Etten-Leur',
+        contact: 'contact2',
         userid: '2'
     },
     {
         id: '3',
-        naam: 'Vijf Eiken',
+        naam: 'naam3',
         adres: 'Rijen',
+        contact: 'contact3',
         userid: '3'
     }
 ];
