01ab1d1d8cf26faa144537afaeee1a96d5bc11c9
Unknown <rick.voesenek@hotmail.com>
2018-05-15 12:30:31 +0200

    /login and /register added

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jwt-simple');
+const auth = require('../auth/authentication');
 const db = require('../database/DBConnector');
-const bodyParser = require('body-parser');
+const regex = require('regex-email');
+const error = require('../errorhandler');
+
+// TEST DATA
+
 const deelnemer = require('../database/deelnemer');
 const maaltijd = require('../database/maaltijd');
 const studentenhuis = require('../database/studentenhuis');
 const user = require('../database/user');
-
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: false }));
 
 // GET requests
 router.get('/studentenhuis/:huisId?', (req, res) => {
@@ -59,12 +62,61 @@ router.get('/studentenhuis/:huisId?/maaltijd/:maaltijdId?', (req, res) => {
 });
 
 // POST Requests
+// Login with the following body {"username":"<username>", "password":"<password>"}
 router.post('/login', (req, res) => {
-    res.send('POST login')
+    let username = req.body.username || '';
+    let password = req.body.password || '';
+
+    db.query('SELECT email, password FROM user WHERE email = ?', [username], (error, rows, fields) => {
+        if(error) {
+            res.status(500).json(error);
+        }
+
+        console.log(rows);
+
+        if (username == rows[0].email && password == rows[0].password) {
+            let token = auth.encodeToken(username);
+            res.status(200).json({
+                "token": token,
+                "status": 200,
+                "parameters": res.body
+            });
+        } else {
+            error.notAuthorized(res);
+        }
+    });
 });
 
+// Register with the follow body {"firstname": "<firstname>","lastname": "<lastname>","email": "<email>","password": "<password>"}
 router.post('/register', (req, res) => {
-    res.send('POST register')
+    let firstname = req.body.firstname || '';
+    let lastname = req.body.lastname || '';
+    let email = req.body.email || '';
+    let password = req.body.password || '';
+    if (firstname !== '' && lastname !== '' && email !== '' && password !== '') {
+        if (regex.test(email) === true) {
+            db.query("SELECT Email FROM user WHERE Email = ?", [email], function(err, result) {
+                if(result.length > 0){
+                    error.emailTaken(res);
+                }
+                else{
+                    db.query("INSERT INTO `user` (Voornaam, Achternaam, Email, Password) VALUES (?, ?, ?, ?)" ,[firstname, lastname, email, password], function(err, result) {
+                        console.log(result);
+                        db.query("SELECT Voornaam, Achternaam, Email FROM user WHERE Email = ?",[email], function(err, result) {
+                            if (err) throw err;
+                            res.json(result)});
+                    });
+                    let token = auth.encodeToken(email);
+                }
+            });
+        }
+        else{
+            error.emailInvalid(res);
+        }
+    }
+    else{
+        error.missingProp(res);
+    }
 });
 
 router.post('/studentenhuis', (req, res) => {
