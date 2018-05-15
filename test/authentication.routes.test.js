b4ee03bed9d41dd2e6da6247a4cc0909dd152468
Unknown <rick.voesenek@hotmail.com>
2018-05-15 21:30:29 +0200

    Test 95% writen

--- test/authentication.routes.test.js
+++ test/authentication.routes.test.js
@@ -1,161 +1,159 @@
 /**
- * Testcases aimed at testing the authentication process.
+ * Testcases aimed at testing the authentication process. 
  */
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../index');
-const db = require('../database/DBConnector');
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../app')
+const db = require('../datasource/dbCon')
 
-chai.should();
-chai.should();
-chai.use(chaiHttp);
+chai.should()
+chai.use(chaiHttp)
 
 // After successful registration we have a valid token. We export this token
 // for usage in other testcases that require login.
-let validToken;
 
 describe('Registration', function () {
-    this.timeout(10000);
-
-    db.query("DELETE FROM user WHERE Email = ?", ['rvoesene@avans.nl']);
+    this.timeout(10000)
 
     it('should return a token when providing valid information', function (done) {
         chai.request(app)
             .post('/api/register')
             .send({
-                "voornaam": "Rick",
-                "achternaam": "Voesenek",
-                "email": "rvoesene@avans.nl",
+                "voornaam": "Yannick",
+                "achternaam": "Willems",
+                "email": "ywillems3@avans.nl",
                 "password": "test123"
             })
             .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
+                res.should.have.status(200)
+                res.body.should.be.a('object')
 
-                validToken = res.body.token;
-                module.exports = {
-                    token: validToken
-                };
+                db.query("DELETE FROM user WHERE Email = ?", ['ywillems3@avans.nl'])
                 done()
             })
-    });
+    })
 
-    it('should return an error on GET request', (done) => {
+    it('should return an error on GET request', function(done) {
         chai.request(app)
             .get('/api/register')
             .end((err, res) => {
-                res.should.have.status(404);
+                res.should.have.status(404)
                 done()
             })
-    });
+    })
 
-    it('should throw an error when the user already exists', (done) => {
+    it('should throw an error when the user already exists', function(done) {
         chai.request(app)
             .post('/api/register')
             .send({
-                "voornaam": "Rick",
-                "achternaam": "Voesenek",
-                "email": "rvoesene@avans.nl",
+                "voornaam": "Jan",
+                "achternaam": "Smit",
+                "email": "jsmit@server.nl",
+                "password": "secret"
+            })
+            .end((err, res) => {
+                res.should.have.status(409)
+                done()
+            })
+    })
+
+    it('should throw an error when no firstname is provided', function(done) {
+        chai.request(app)
+            .post('/api/register')
+            .send({
+                "achternaam": "Willems",
+                "email": "ywillems3@avans.nl",
                 "password": "test123"
             })
             .end((err, res) => {
-                res.should.have.status(409);
+                res.should.have.status(412)
                 done()
             })
-    });
+    })
 
-    it('should throw an error when no firstname is provided', (done) => {
+    it('should throw an error when firstname is shorter than 2 chars', function(done) {
         chai.request(app)
             .post('/api/register')
             .send({
-                "achternaam": "Voesenek",
-                "email": "rvoesene@avans.nl",
+                "voornaam": "Y",
+                "achternaam": "Willems",
+                "email": "ywillems3@avans.nl",
                 "password": "test123"
             })
             .end((err, res) => {
-                res.should.have.status(412);
+                res.should.have.status(412)
                 done()
             })
-    });
+    })
 
-    it('should throw an error when firstname is shorter than 2 chars', (done) => {
+    it('should throw an error when no lastname is provided', function(done) {
         chai.request(app)
             .post('/api/register')
             .send({
-                "voornaam": "R",
-                "achternaam": "Voesenek",
-                "email": "rvoesene@avans.nl",
+                "voornaam": "Yannick",
+                "email": "ywillems3@avans.nl",
                 "password": "test123"
             })
             .end((err, res) => {
-                res.should.have.status(412);
+                res.should.have.status(412)
                 done()
             })
-    });
+    })
 
-    it('should throw an error when no lastname is provided', (done) => {
+    it('should throw an error when lastname is shorter than 2 chars', function(done) {
         chai.request(app)
             .post('/api/register')
             .send({
-                "voornaam": "Rick",
-                "email": "rvoesene@avans.nl",
+                "voornaam": "Yannick",
+                "achternaam": "W",
+                "email": "ywillems3@avans.nl",
                 "password": "test123"
             })
             .end((err, res) => {
-                res.should.have.status(412);
+                res.should.have.status(412)
                 done()
             })
-    });
+    })
 
-    it('should throw an error when lastname is shorter than 2 chars', (done) => {
+    it('should throw an error when email is invalid', function(done) {
         chai.request(app)
             .post('/api/register')
             .send({
-                "voornaam": "Rick",
-                "achternaam": "V",
-                "email": "rvoesene@avans.nl",
-                "password": "test123"
-            })
-            .end((err, res) => {
-                res.should.have.status(412);
-                done()
-            })
-    });
-
-    it('should throw an error when email is invalid', (done) => {
-        chai.request(app)
-            .post('/api/register')
-            .send({
-                "voornaam": "Rick",
-                "achternaam": "Voesenek",
+                "voornaam": "Yannick",
+                "achternaam": "Willems",
                 "email": "test",
                 "password": "test123"
             })
             .end((err, res) => {
-                res.should.have.status(409);
+                res.should.have.status(409)
                 done()
             })
     })
-});
+})
 
 describe('Login', function () {
-    this.timeout(10000);
+    this.timeout(10000)
 
-    it('should return a token when providing valid information', (done) => {
+    it('should return a token when providing valid information', function(done) {
         chai.request(app)
             .post('/api/login')
             .send({
-                "email": "rvoesene@avans.nl",
-                "password": "test123"
+                "email": "jsmit@server.nl",
+                "password": "secret"
             })
             .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
+                res.should.have.status(200)
+                res.body.should.be.a('object')
+
+                let validToken = res.body.token
+                module.exports = {
+                    token: validToken
+                }
                 done()
             })
-    });
+    })
 
-    it('should throw an error when email does not exist', (done) => {
+    it('should throw an error when email does not exist', function(done) {
         chai.request(app)
             .post('/api/login')
             .send({
@@ -163,34 +161,34 @@ describe('Login', function () {
                 "password": "test123"
             })
             .end((err, res) => {
-                res.should.have.status(404);
+                res.should.have.status(404)
                 done()
             })
-    });
+    })
 
-    it('should throw an error when email exists but password is invalid', (done) => {
+    it('should throw an error when email exists but password is invalid', function(done) {
         chai.request(app)
             .post('/api/login')
             .send({
-                "email": "rvoesene@avans.nl",
+                "email": "jsmit@server.nl",
                 "password": "test"
             })
             .end((err, res) => {
-                res.should.have.status(401);
+                res.should.have.status(401)
                 done()
             })
-    });
+    })
 
-    it('should throw an error when using an invalid email', (done) => {
+    it('should throw an error when using an invalid email', function(done) {
         chai.request(app)
             .post('/api/login')
             .send({
                 "email": "test",
-                "password": "test123"
+                "password": "secret"
             })
             .end((err, res) => {
-                res.should.have.status(409);
+                res.should.have.status(409)
                 done()
             })
     })
-});
+})
