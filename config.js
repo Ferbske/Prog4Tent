217605cf574e4fa395310166e576a3dd8fcb89ce
Unknown <rick.voesenek@hotmail.com>
2018-05-15 12:27:57 +0200

    config JS created with universal configs

---
+++ config.js
@@ -0,0 +1,11 @@
+// const config = {
+//     port: 666 || process.env.PORT,
+//     secretkey: 'k6S7N-jm:2C]qTM:'
+// };
+//
+// module.exports = config;
+
+module.exports = {
+    port: 666,
+    secretkey: 'k6S7N-jm:2C]qTM:'
+};
