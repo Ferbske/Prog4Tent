cddd89ef4bd2c745c90c25d2da9831beedba0cfd
Unknown <rick.voesenek@hotmail.com>
2018-05-16 18:15:26 +0200

    Port changed for Heroku

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 // module.exports = config;
 
 module.exports = {
-    port: 666,
+    port: process.env.PORT || 666,
     secretkey: 'k6S7N-jm:2C]qTM:',
     token: "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjczMjg1NjgsImlhdCI6MTUyNjQ2NDU2OCwic3ViIjoianNtaXRAc2VydmVyLm5sIn0.USNDN6DcFOme0MsognMRVaZktg2BvZ9evLhMZq4HeLE"
 };
